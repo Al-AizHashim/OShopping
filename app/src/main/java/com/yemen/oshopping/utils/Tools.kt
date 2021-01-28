9afa7d8d67bea396adb5e18bbd0c977744a89cb9
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2021-01-28 14:01:05 +0300

    Some issues are fixed

--- app/src/main/java/com/yemen/oshopping/utils/Tools.kt
+++ app/src/main/java/com/yemen/oshopping/utils/Tools.kt
@@ -13,7 +13,7 @@ class Tools {
             val window = act.window
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.statusBarColor = act.resources.getColor(R.color.colorPrimaryDark2)
+            window.statusBarColor = act.resources.getColor(R.color.colorPrimaryDark)
         }
     }
 
